6b27bc5ad037c20ecd4d22ae79d4693a2235099f
Mikhail Sauchuk <mishasavchuk@gmail.com>
2020-03-22 14:05:24 +0300

    feat: add functionality to basic check of the input from user; make style refactoring

--- HW01/Mikhail_Sauchuk/mikhai_sauchuk_hw_01_t_01.rb
+++ HW01/Mikhail_Sauchuk/mikhai_sauchuk_hw_01_t_01.rb
@@ -4,16 +4,40 @@ ONE_MIN_ANGLE = 360 / 60
 ONE_OUR_ANGLE = ONE_MIN_ANGLE * 5
 
 def calc_angle
-    print "Please, input hours "
-    hours = get_input
-    print "Now, please input minutes "
-    minutes = get_input
-    angle = (minutes * ONE_MIN_ANGLE - (hours * ONE_OUR_ANGLE + minutes / 2)).abs
-    puts ("The angel between hours hand #{hours} and minutes #{minutes} is #{angle}")
+  print 'Please, input hours '
+  hours = clear_hours
+  print 'Now, please input minutes '
+  minutes = clear_minutes
+  angle = (minutes * ONE_MIN_ANGLE - (hours * ONE_OUR_ANGLE + minutes / 2)).abs
+  puts "The angel between hours hand #{hours.to_i} and minutes #{minutes.to_i}"\
+  " is #{angle}"
 end
 
-def get_input
-    return gets.to_f
+def take_input
+  gets.to_f
+end
+
+def clear_hours
+  # this method take input from user and check if the input hours is valid
+  # and convert hours if hours in 24-hours format
+  hours = take_input
+  while hours > 23
+    puts 'Invalid input, please input hours beetwee 0 and 23'
+    hours = take_input
+  end
+  hours -= 12 if hours >= 12
+  hours
+end
+
+def clear_minutes
+  # this method take input from user and check if the input minutes is valid
+  minutes = take_input
+  while minutes > 60
+    puts 'Invalid input, please input mintes beetwee 0 and 60'
+    minutes = take_input
+  end
+  minutes = 0 if minutes == 60
+  minutes
 end
 
 calc_angle
