90a4eaf0735c828931713318cdc9d480aa84edbf
Mikhail Sauchuk <mishasavchuk@gmail.com>
2020-04-04 20:44:21 +0300

    fix: remove redundant file

--- HW03/Mikhail_Sauchuk/lecture_31_03.rb
+++
@@ -1,9 +0,0 @@
-# думайте как конечные пользователь вашего класса
-# solid
-# s - single responsobility
-# this & that & other
-# huge public interface
-# open/close - можно расширять класс (создавать новые классы) но не изменять методы в самом классе
-# liskov substitution principle
-# Interface segregation
-# Dependency inversion
