bf8ae7f1a92f2f0bcaf7d1a227adaa367d533320
Mikhail Sauchuk <mishasavchuk@gmail.com>
2020-04-08 00:36:43 +0300

    feat: add unit tests for methods in my_array

---
+++ HW04/Mikhail_Sauchuk/my_array_spec.rb
@@ -0,0 +1,49 @@
+# write specs for each of methods from my_array.rb usign RSpec
+
+require 'rspec'
+require_relative 'my_array.rb'
+
+RSpec.describe Array do
+  subject { described_class.new([1, 2, 3]) }
+
+  describe '#my_each' do
+    context 'if block was given' do
+      it 'return values from array one by one' do
+        expect do
+          subject.my_each { |elem| puts elem }
+        end.to output("1\n2\n3\n").to_stdout
+      end
+    end
+    context 'if no block' do
+      it 'retun initial array' do
+        expect(subject.my_each).to eql([1, 2, 3])
+      end
+    end
+  end
+
+  describe '#my_map' do
+    context 'if block was given' do
+      it 'return updated array' do
+        expect(subject.my_map { |elem| elem * 2 }).to eql([2, 4, 6])
+      end
+    end
+    context 'if no block' do
+      it 'retun empty array' do
+        expect(subject.my_map).to eql([])
+      end
+    end
+  end
+
+  describe '#my_select' do
+    context 'if block was given' do
+      it 'return filtered array' do
+        expect(subject.my_select { |elem| elem > 1 }).to eql([2, 3])
+      end
+    end
+    context 'if no block' do
+      it 'retun Enumerator object' do
+        expect(subject.my_select).to be_an Enumerator
+      end
+    end
+  end
+end
