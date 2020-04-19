ce267de985468960dfc7aa097565bf96b417948e
Mikhail Sauchuk <mishasavchuk@gmail.com>
2020-04-19 21:58:43 +0300

    refactor: update rudocop-rscpec issues

--- HW04/Mikhail_Sauchuk/my_array_spec.rb
+++ HW04/Mikhail_Sauchuk/my_array_spec.rb
@@ -1,55 +1,55 @@
 # write specs for each of methods from my_array.rb usign RSpec
+# rubocop:disable Metrics/BlockLength
 
 require 'rspec'
 require_relative 'my_array.rb'
 
 RSpec.describe Array do
-  subject { described_class.new([1, 2, 3]) }
+  subject(:test_array) { described_class.new([1, 2, 3]) }
 
   describe '#my_each' do
-    context 'if block was given' do
+    context 'when block was given' do
       it 'return values from array one by one' do
         expect do
-          subject.my_each { |elem| puts elem }
+          test_array.my_each { |elem| puts elem }
         end.to output("1\n2\n3\n").to_stdout
       end
     end
-    context 'if no block' do
+
+    context 'when no block' do
       it 'retun initial array' do
-        expect(subject.my_each).to eql([1, 2, 3])
+        expect(test_array.my_each).to eql([1, 2, 3])
       end
     end
   end
-end
 
-RSpec.describe Array do
-  subject { described_class.new([1, 2, 3]) }
   describe '#my_map' do
-    context 'if block was given' do
+    context 'when block was given' do
       it 'return updated array' do
-        expect(subject.my_map { |elem| elem * 2 }).to eql([2, 4, 6])
+        expect(test_array.my_map { |elem| elem * 2 }).to eql([2, 4, 6])
       end
     end
-    context 'if no block' do
+
+    context 'when no block' do
       it 'retun empty array' do
-        expect(subject.my_map).to eql([])
+        expect(test_array.my_map).to eql([])
+      end
+    end
+  end
+
+  describe '#my_select' do
+    context 'when block was given' do
+      it 'return filtered array' do
+        expect(test_array.my_select { |elem| elem > 1 }).to eql([2, 3])
+      end
+    end
+
+    context 'when no block' do
+      it 'retun Enumerator object' do
+        expect(test_array.my_select).to be_an Enumerator
       end
     end
   end
 end
 
-RSpec.describe Array do
-  subject { described_class.new([1, 2, 3]) }
-  describe '#my_select' do
-    context 'if block was given' do
-      it 'return filtered array' do
-        expect(subject.my_select { |elem| elem > 1 }).to eql([2, 3])
-      end
-    end
-    context 'if no block' do
-      it 'retun Enumerator object' do
-        expect(subject.my_select).to be_an Enumerator
-      end
-    end
-  end
-end
+# rubocop:enable  Metrics/BlockLength
